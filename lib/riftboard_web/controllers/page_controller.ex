13b697bc04eefd7611868072d296f73544b0b8bd
AdrianLambertt <adrian.lambertt2@gmail.com>
2026-07-11 13:19:09 +0100

    (fix)  logged in user has same baseline as log out on header, logged out user does not force redirect

--- lib/riftboard_web/controllers/page_controller.ex
+++ lib/riftboard_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule RiftboardWeb.PageController do
   use RiftboardWeb, :controller
 
   def home(conn, _params) do
-    redirect(conn, to: ~p"/boards")
+    if conn.assigns[:current_user] do
+      redirect(conn, to: ~p"/boards")
+    else
+      redirect(conn, to: ~p"/users/log_in")
+    end
   end
 end
--- test/riftboard_web/controllers/page_controller_test.exs
+++ test/riftboard_web/controllers/page_controller_test.exs
@@ -1,8 +1,15 @@
 defmodule RiftboardWeb.PageControllerTest do
   use RiftboardWeb.ConnCase
 
-  test "GET / redirects to /boards", %{conn: conn} do
-    conn = get(conn, ~p"/")
+  import Riftboard.AccountsFixtures
+
+  test "GET / redirects to /boards when logged in", %{conn: conn} do
+    conn = conn |> log_in_user(user_fixture()) |> get(~p"/")
     assert redirected_to(conn) == ~p"/boards"
+  end
+
+  test "GET / redirects to /users/log_in when logged out", %{conn: conn} do
+    conn = get(conn, ~p"/")
+    assert redirected_to(conn) == ~p"/users/log_in"
   end
 end
